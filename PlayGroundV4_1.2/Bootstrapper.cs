8e0fdbf75b7275f3cf5efc8dd4655e0009228804
Davide <davide.formica@bernina.com>
2018-11-26 11:07:15 +0100

    Save molesting v2

---
+++ PlayGroundV4_1.2/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PlayGroundV4_1._2
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        #region Constructor
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        #endregion
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+    }
+}
